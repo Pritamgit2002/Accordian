7ebe00d36bd8ecf15e1b15ac700227695a2e6cfe
Pritam Gain <pritamgain@Pritams-Mac-mini.local>
2025-07-31 00:32:43 +0530

    Refactor layout and accordion components to use DM Sans font; add SVG icons

--- src/components/accordian.tsx
+++ src/components/accordian.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { data } from "@/constants";
+import Image from "next/image";
 import React, { useState } from "react";
 
 export const Accordian = () => {
@@ -15,15 +16,15 @@ export const Accordian = () => {
 
   return (
     <main className=" max-w-full h-max flex flex-col items-center justify-center gap-32 mt-40 ">
-      <span className=" text-[#8800C8] font-bold text-5xl ">
+      <span className=" text-[#8800C8] font-dm-sans font-bold text-5xl ">
         Frequently asked questions
       </span>
-      <div className=" w-[1104px] h-80 max-h-max flex flex-col gap-4 p-4  rounded-lg">
+      <div className=" w-[1104px] h-max max-h-max flex flex-col gap-4 rounded-lg border-t-[2px] pt-4 border-gray-200 ">
         {data.map((item, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col border-y-[2px] border-gray-200 gap-4 py-2"
+              className="flex flex-col border-b-[2px] border-gray-200 gap-6 pb-6  font-dm-sans"
             >
               <div
                 className="flex flex-row items-center justify-between cursor-pointer"
@@ -33,8 +34,24 @@ export const Accordian = () => {
                   <span>Q{item.id}. </span>
                   <span className="">{item.question}</span>
                 </div>
-                <button className=" text-4xl font-bold text-[#252525] ">
-                  {open.includes(item.id) ? "-" : "+"}
+                <button className=" text-4xl font-bold text-[#252525] transition-all duration-300 ease-in-out">
+                  {open.includes(item.id) ? (
+                    <Image
+                      src="/minus.svg"
+                      alt="minus"
+                      width={20}
+                      height={20}
+                      className=" "
+                    />
+                  ) : (
+                    <Image
+                      src="/plus.svg"
+                      alt="minus"
+                      width={20}
+                      height={20}
+                      className=" "
+                    />
+                  )}
                 </button>
               </div>
               {open.includes(item.id) && (
